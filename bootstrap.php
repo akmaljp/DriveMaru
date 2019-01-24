738174a3c54d09b93386102729ea9b112e04b20c
akmaljp <46837901+akmaljp@users.noreply.github.com>
2019-01-24 18:55:58 +0700

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -21,7 +21,7 @@ function subscribe(Dispatcher $events)
 				'drivemaru',
 				[
 					'host'    => 'drive.bisnison.store',
-					'extract' => "!drive\.bisnison\.store\/video\/(?'id'[-0-9A-Z_a-z]+)!",
+					'extract' => "!drive\.bisnison\.store/(?:video/)(?'id'[-0-9A-Z_a-z]+)!",
 					'iframe'  => ['src' => 'http://drive.bisnison.store/video/{@id}']
 				]
 			);
